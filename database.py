0b0547c0c640f6a768d1f86328a0d6e90e55da5e
toki513 <tokitahmidkhan@gmail.com>
2026-03-15 21:23:55 +0600

    updated

--- database.py
+++ database.py
@@ -1,17 +1,22 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker,DeclarativeBase
+
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker,create_async_engine
 
 
-DATABASE_URL = "postgresql://postgres:123456@localhost:5432/postgres"
+DATABASE_URL = "postgresql+asyncpg://postgres:123456@localhost:5432/postgres"
 
-engine = create_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit = False, autoflush=False,bind=engine)
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_ = AsyncSession,
+    expire_on_commit=False,
+)
 
 class Base(DeclarativeBase):
     pass
 
 
-def get_db():
-    with SessionLocal() as db:
-        yield db
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
