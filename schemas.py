1322ca1a5129376fbfb8563fbc63932321864b28
toki513 <tokitahmidkhan@gmail.com>
2026-04-03 16:47:22 +0600

    updated

--- schemas.py
+++ schemas.py
@@ -55,7 +55,7 @@ class PostUpdate(BaseModel):
 class PostResponse(PostBase):
     model_config=ConfigDict(from_attributes=True)
     
-    
+    id:int
     user_id:int
     date_posted:datetime
     author:UserPublic
